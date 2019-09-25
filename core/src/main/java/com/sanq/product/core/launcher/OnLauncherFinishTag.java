bff4fa9c25838769d20bb3c906a5ee20a45c4176
xiezhyan <bin_cruel@sohu.com>
2019-09-25 16:48:33 +0800

    调整

--- core/src/main/java/com/sanq/product/core/launcher/OnLauncherFinishTag.java
+++ core/src/main/java/com/sanq/product/core/launcher/OnLauncherFinishTag.java
@@ -3,7 +3,5 @@ package com.sanq.product.core.launcher;
 public enum OnLauncherFinishTag {
     SIGNED,         //登录
     NOT_SIGNED,      //未登录
-
-    IS_VALIDATE,   //是否已经输入邀请码
-    IS_TO_HOME      //是否进入首页
+    TO_HOME      //进入首页
 }
--- core/src/main/java/com/sanq/product/core/mvp/proxy/IProxy.java
+++
@@ -1,9 +0,0 @@
-package com.sanq.product.core.mvp.proxy;
-
-import com.sanq.product.core.mvp.presenter.BasePresenter;
-
-public interface IProxy {
-    void bindPreserent();
-
-    void unBindPreserent();
-}
