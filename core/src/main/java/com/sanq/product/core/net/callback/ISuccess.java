d4435f4cce5ccb44274045b72b4679a83e6e2cc8
xiezhyan <xiezhyan@126.com>
2020-03-01 19:00:02 +0800

    .

--- core/src/main/java/com/sanq/product/core/net/callback/ISuccess.java
+++ core/src/main/java/com/sanq/product/core/net/callback/ISuccess.java
@@ -1,5 +1,9 @@
 package com.sanq.product.core.net.callback;
 
+import org.json.JSONObject;
+
 public interface ISuccess {
     void onSuccess(String response);
+
+    void onSuccess(JSONObject response);
 }
